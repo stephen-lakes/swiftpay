064dd8fee25eaec37ef80c301944f98bf962b128
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-26 22:18:49 +0100

    - Set server port to 3000 and added console log for server running status
    	- Updated server port configuration to use environment variable
    	PORT with fallback to 3000

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post("/users/login", async (request, response) => {
 // User Endpoints
 app.get("/users/balance", () => {});
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port 3000`);
+  console.log(`Server running on port ${PORT}`);
 });
