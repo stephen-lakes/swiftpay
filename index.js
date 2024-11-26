2d4fd99dd998e2a130a27c04ac47c48df75348bc
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-26 23:37:04 +0100

    import uuid version 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
 let users = [];
