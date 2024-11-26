0b69a17d789589225919066ab066322c9ad66487
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-26 09:57:57 +0100

    Implement endpoint to regsite new users     - import `bcrypt` package     - create a `users` to store users     - configure app to use express.json()     - implement logic to register new user     - add route to get user balance

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
+let users = [];
 const app = express();
 
+app.use(express.json());
 app.get("/", (req, res) => {
   console.log("Home Route Accessed");
   res.send("Hello WOrld!");
 });
 
 // User Registration/Sign-up Endpoint
-app.post("/users/register", () => {});
+app.post("/users/register", (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+    if (!name || !email || !password)
+      response.status(400).json("name, email and password is required");
+    const hashedPassword = bcrypt.hash(password, 10);
+    const newUser = { name, email, balance: 10000 };
+
+    users = users.concat(newUser);
+
+    response.status(201).json({ message: "user registered", user: newUser });
+  } catch (error) {
+    response.status(500).json({ error: "Failed to register user" });
+  }
+});
 
 // User Login/Sing-in Endpoint
 app.post("/users/login", () => {});
 
 // User Endpoints
-app.get("/users/balance", ()=>{})
-
+app.get("/users/balance", () => {});
 
 const PORT = 3000;
 app.listen(PORT, () => {
