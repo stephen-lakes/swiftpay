a121d55b25917475b2cdeba80c6aba18927214ae
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-11-26 08:44:55 +0100

    feat: Add user registration, login, and balance endpoints
    - Implemented POST /users/register for user registration
    - Implemented POST /users/login for user login
    - Implemented GET /users/balance to fetch user balance

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
   res.send("Hello WOrld!");
 });
 
-app.listen(3000, () => {
+// User Registration/Sign-up Endpoint
+app.post("/users/register", () => {});
+
+// User Login/Sing-in Endpoint
+app.post("/users/login", () => {});
+
+// User Endpoints
+app.get("/users/balance", ()=>{})
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
   console.log(`Server running on port 3000`);
 });
