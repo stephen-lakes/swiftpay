1e01a8ea8c3727e8680e01fccd414437c514d756
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-01 22:40:36 +0100

    fix: add timestamps property in the correct position

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,35 +1,37 @@
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
 
-const transactionSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: uuidv4,
+const transactionSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: uuidv4,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    senderId: {
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "completed", "failed"],
+      default: "pending",
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: false,
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  senderId: {
-    type: String,
-    required: true,
-  },
-  receiverId: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "completed", "failed"],
-    default: "pending",
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: false,
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
