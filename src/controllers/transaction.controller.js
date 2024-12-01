2f8ca314e60a7ff490dbb4af71e1b80734cb305a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-01 19:50:57 +0100

    feat: Add transaction controllers
    - Defined controller functions for various transaction operations:
      - getAllTransactions
      - getTransactionById
      - getTransactionsByUserId
      - getSuccessfulTransactions
      - getFailedTransactions
    - Organized and exported all controller functions.

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,19 @@
+const Transaction = require("../models/transaction.model");
+
+const getAllTransactions = async (req, res) => {};
+
+const getTransactionById = async (req, res) => {};
+
+const getTransactionsByUserId = async (req, res) => {};
+
+const getSuccessfulTransactions = async (req, res) => {};
+
+const getFailedTransactions = async (req, res) => {};
+
+module.exports = {
+  getAllTransactions,
+  getTransactionById,
+  getTransactionsByUserId,
+  getSuccessfulTransactions,
+  getFailedTransactions,
+};
