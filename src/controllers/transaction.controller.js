12fae293191e5428d49a7521b3e54f1bf8887c92
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-01 19:58:52 +0100

    feat: Implement logic to retrieve all transactions
    - Added functionality to fetch all transactions.
    - Included response handling for successful data retrieval.
    - Added error handling for failed transaction fetch attempts.

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,14 +1,21 @@
 const Transaction = require("../models/transaction.model");
 
-const getAllTransactions = async (req, res) => {};
+const getAllTransactions = async (request, response) => {
+  try {
+    const transactions = await Transaction.find();
+    response.status(200).json({ message: "SUCCESS", data: transactions });
+  } catch (error) {
+    response.status(500).json({ message: "Failed to fecth transactions" });
+  }
+};
 
-const getTransactionById = async (req, res) => {};
+const getTransactionById = async (request, response) => {};
 
-const getTransactionsByUserId = async (req, res) => {};
+const getTransactionsByUserId = async (request, response) => {};
 
-const getSuccessfulTransactions = async (req, res) => {};
+const getSuccessfulTransactions = async (request, response) => {};
 
-const getFailedTransactions = async (req, res) => {};
+const getFailedTransactions = async (request, response) => {};
 
 module.exports = {
   getAllTransactions,
