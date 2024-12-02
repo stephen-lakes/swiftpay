61951ee771afb1804d14288d570c69866846d38a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-02 16:44:55 +0100

    extract token from the bearer authorization

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,13 @@
 const authenticateToken = (request, response, next) => {
-  const token = request.header("Authorization");
+  const authHeader = request.header["Authorization"];
+  if (!authHeader) return response.status(401).json({ error: "Unauthorized" });
+
+  const token = authHeader.split(" ")[1]; // Extract the token from the "Bearer <token>" format
+
   if (!token) return response.status(401).json({ error: "Unauthorized" });
 
   jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
     if (error) return response.status(403).json({ error: "Invalid Token" });
-
     request.user = user; // Attach user info to the request
     next();
   });
