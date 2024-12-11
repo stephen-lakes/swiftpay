0da7a8669d40a73e85b325fb05ca28bda1297588
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-11 12:29:18 +0100

    feat: Add SignUpDto for user registration validation
    - Created SignUpDto class for validating user registration data
    - Added class-validator decorators for input validation
    - Ensured firstName and lastName are not empty and have a length between 2 and 30 characters
    - Ensured email is not empty and is a valid email format
    - Made phoneNumber optional with a length between 10 and 15 characters
    - Ensured password is not empty and has a minimum length of 6 characters

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  Length,
+  MinLength,
+} from "class-validator";
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @Length(2, 30)
+  firstName: string;
+
+  @IsNotEmpty()
+  @Length(2, 30)
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @Length(10, 15)
+  phoneNumber?: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
