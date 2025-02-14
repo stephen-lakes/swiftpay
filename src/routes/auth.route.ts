65b081cf9a307e91eb2104e6ce3d4afa2fade92d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-14 16:08:41 +0100

    implemented Auth route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,46 @@
-import express, { Request, Response } from "express";
+import express, { Request, response, Response, Router } from "express";
+import { SignUpDto } from "../dtos/auth.dto.ts";
+import { validate } from "class-validator";
+import { User } from "../entities/user.entity.ts";
+import { AppDataSource } from "../config/database.config.ts";
 
-const router = express.Router();
+const router: Router = express.Router();
 
 router.post("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "AUTH endpoint" });
 });
 // User Registration
-router.post("/sign-up", (req: Request, res: Response) => {
-  res.status(200).json({ message: "sign-up endpint" });
+router.post("/sign-up", async (req: Request, res: Response) => {
+  try {
+    const signUpData = new SignUpDto(req.body);
+    const errors = await validate(signUpData);
+
+    if (errors.length > 0) {
+      const detailedErrors = errors.map((err) => ({
+        property: err.property,
+        constraints: err.constraints,
+      }));
+      res
+        .status(400)
+        .json({ message: "Sign up validation failed", errors: detailedErrors });
+      return;
+    }
+    // Proceed with business logic if validation succeeds
+    AppDataSource.getRepository(User);
+
+    const userRepository = AppDataSource.getRepository(User);
+    const newUser = userRepository.create(signUpData);
+    await userRepository.save(newUser);
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: signUpData });
+  } catch (error) {
+    console.error("Sign-up error:", error);
+    res
+      .status(500)
+      .json({ message: "Failed to Sign up", error: error.message });
+  }
 });
 
 // User login
