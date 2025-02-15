0c17681a8c1e041f5b2a5abafa0fe3cb44189af4
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-15 21:04:30 +0100

    Implement custom response

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,10 +1,25 @@
+import { Response } from "express";
+
+export type ResponseType = {
+  status: `success` | `error` | `failed`;
+  message: string;
+  code: number;
+  data?: any; // Optional data property
+};
+
 export const Utility = {
-  sendResponse: (request: any, response: any) => {
-    return {
-      message: "",
-      data: {},
-      status: "",
-      code: "",
-    };
+  sendResponse: (
+    res: Response,
+    data?: ResponseType
+  ) => {
+    const response: ResponseType = {
+      status: data?.status || `success`,
+      message: data?.message || `success`,
+      code: data?.code || 200,
+      data: data?.data
+    }
+
+    res.status(response.code).json(response)
   },
+  
 };
