e38c95e65e6580d57c2e0ccde6a213e0ec0b1683
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-18 03:14:46 +0100

    database config

--- src/database.config.ts
+++ src/database.config.ts
@@ -1,29 +1,30 @@
-import "reflect-metadata";
-import dotenv from "dotenv";
-dotenv.config();
-import { DataSource } from "typeorm";
+import path from "path";
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
-import { User } from "./entities/user.entity.ts";
-import path, { dirname } from "path";
-import { Transaction } from "./entities/transaction.entity.ts";
+dotenv.config();
 
-const connectionOptions: PostgresConnectionOptions = {
-  type: process.env.DB_TYPE as "postgres",
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+export const connectionOptions: PostgresConnectionOptions = {
+  name: "default",
+  type: "postgres",
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: true,
+  synchronize:
+    process.env.DB_SYNCRHONIZE == "false" ? false : process.env.ENV == "dev",
   logging: false,
-  // entities: ["./entities/*{.ts,.js}"],
-  migrations: ["src/migrations/*{.ts,.js}"],
-  // migrations: [path.join(__dirname, "../migrations/*{.ts,.js}")],
-  // "subscribers": ["src/subscriber/*.js"]
-  entities: [User, Transaction],
+  entities: [path.join(__dirname, "src/**/*.entity{.ts,.js}")],
+  migrations: [path.join(__dirname, "src/**/*.migration{.ts,.js}")],
+  subscribers: [path.join(__dirname, "src/**/*.subscriber{.ts,.js}")],
 };
-// Initialize DataSource
-export const AppDataSource = new DataSource(connectionOptions);
 
-// export default AppDataSource;
+export const AppDataSource = new DataSource(connectionOptions);
