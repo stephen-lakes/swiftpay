84c078cdec8ea51178ee5edea7e8c86fa841554b
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-03-26 16:00:26 +0100

    add more user information to user interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -2,8 +2,11 @@ export interface IUser {
   id: string;
   firstName: string;
   lastName: string;
+  middleName?: string;
   email: string;
   phoneNumber?: string;
+  gender: string;
+  dob: string;
   password: string;
   role: string;
   isVerified: boolean;
